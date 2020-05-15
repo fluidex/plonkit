478beeff59e9ef6514b6c3380bdedcafd1ca747f
poma <semenov.roma@gmail.com>
2020-05-15 13:13:56 +0300

    code style

--- src/verifier_groth.sol
+++ src/verifier_groth.sol
@@ -34,7 +34,7 @@ library Pairing {
     }
 
     /*
-     * @return The negation of p, i.e. p.plus(p.negate()) should be zero.
+     * @return The negation of p, i.e. p.plus(p.negate()) should be zero
      */
     function negate(G1Point memory p) internal pure returns (G1Point memory) {
         // The prime q in the base field F_q for G1
@@ -70,18 +70,20 @@ library Pairing {
 
     /*
      * @return r the product of a point on G1 and a scalar, i.e.
-     *         p == p.scalar_mul(1) and p.plus(p) == p.scalar_mul(2) for all
+     *         p == p.scalarMul(1) and p.plus(p) == p.scalarMul(2) for all
      *         points p.
      */
-    function scalar_mul(G1Point memory p, uint256 s) internal view returns (G1Point memory r) {
+    function scalarMul(G1Point memory p, uint256 s) internal view returns (G1Point memory r) {
         uint256[3] memory input = [p.X, p.Y, s];
         bool success;
+
         // solium-disable-next-line security/no-inline-assembly
         assembly {
             success := staticcall(sub(gas(), 2000), 7, input, 0x80, r, 0x60)
             // Use "invalid" to make gas estimation work
             switch success case 0 { invalid() }
         }
+
         require(success, "pairing-mul-failed");
     }
 
@@ -118,7 +120,6 @@ library Pairing {
         }
 
         require(success, "pairing-opcode-failed");
-
         return out[0] != 0;
     }
 }
@@ -153,25 +154,21 @@ contract Verifier {
         uint256[<%vk_input_length%>] memory input
     ) public view returns (bool) {
         uint256[8] memory p = abi.decode(proof, (uint256[8]));
-
-        // Make sure that each element in the proof is less than the prime q
         for (uint8 i = 0; i < p.length; i++) {
+            // Make sure that each element in the proof is less than the prime q
             require(p[i] < PRIME_Q, "verifier-proof-element-gte-prime-q");
         }
-
         Pairing.G1Point memory proofA = Pairing.G1Point(p[0], p[1]);
         Pairing.G2Point memory proofB = Pairing.G2Point([p[2], p[3]], [p[4], p[5]]);
         Pairing.G1Point memory proofC = Pairing.G1Point(p[6], p[7]);
 
         VerifyingKey memory vk = verifyingKey();
-
-        // Compute the linear combination vk_x
-        Pairing.G1Point memory vk_x = vk.IC[0];
-
-        // Make sure that every input is less than the snark scalar field
+        // Compute the linear combination vkX
+        Pairing.G1Point memory vkX = vk.IC[0];
         for (uint256 i = 0; i < input.length; i++) {
-            require(input[i] < SNARK_SCALAR_FIELD, "verifier-gte-snark-scalar-field");
-            vk_x = Pairing.plus(vk_x, Pairing.scalar_mul(vk.IC[i + 1], input[i]));
+            // Make sure that every input is less than the snark scalar field
+            require(input[i] < SNARK_SCALAR_FIELD, "verifier-input-gte-snark-scalar-field");
+            vkX = Pairing.plus(vkX, Pairing.scalarMul(vk.IC[i + 1], input[i]));
         }
 
         return Pairing.pairing(
@@ -179,7 +176,7 @@ contract Verifier {
             proofB,
             vk.alfa1,
             vk.beta2,
-            vk_x,
+            vkX,
             vk.gamma2,
             proofC,
             vk.delta2
