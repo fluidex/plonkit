9b10763be0c31c624d11db6609ae870c25d22757
Koh Wei Jie <weijiekoh@users.noreply.github.com>
2020-12-01 02:38:36 +0000

    Lock in circom and snarkjs version via package.json, and a minor tweak to the poseidon test (#10)
    * locked circom and snarkjs versions; updated process_circom_circuit.sh to use latest snarkjs commands; commented out line that post-processes the witness as it is no longer needed
    
    * include package.json and package-lock.json
    
    * updated README npm instructions

--- test_poseidon_plonk.sh
+++ test_poseidon_plonk.sh
@@ -8,7 +8,7 @@ SETUP_DIR=$DIR"/keys/setup"
 # from zksync/infrastructure/zk/src/run/run.ts
 echo "Step1: download universal setup file"
 pushd keys/setup
-axel -c https://universal-setup.ams3.digitaloceanspaces.com/setup_2^20.key || true
+axel -ac https://universal-setup.ams3.digitaloceanspaces.com/setup_2^20.key || true
 popd
 
 echo "Step2: compile circuit and calculate witness using snarkjs"
