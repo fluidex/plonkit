d4eca48d7cdeea3df9bb1dbffa488e71fe95d6ca
HAOYUatHZ <haoyu@protonmail.com>
2021-02-04 16:55:37 +0800

    minor

--- src/circom_circuit.rs
+++ src/circom_circuit.rs
@@ -7,6 +7,7 @@ use std::collections::BTreeMap;
 use std::str;
 
 use bellman_ce::{
+    plonk::better_better_cs,
     pairing::{ff::PrimeField, ff::ScalarEngine, Engine},
     Circuit, ConstraintSystem, Index, LinearCombination, SynthesisError, Variable,
 };
@@ -74,6 +75,73 @@ impl<'a, E: Engine> CircomCircuit<E> {
 impl<'a, E: Engine> Circuit<E> for CircomCircuit<E> {
     //noinspection RsBorrowChecker
     fn synthesize<CS: ConstraintSystem<E>>(self, cs: &mut CS) -> Result<(), SynthesisError> {
+        let witness = &self.witness;
+        let wire_mapping = &self.wire_mapping;
+        for i in 1..self.r1cs.num_inputs {
+            cs.alloc_input(
+                || format!("variable {}", i),
+                || {
+                    Ok(match witness {
+                        None => E::Fr::from_str("1").unwrap(),
+                        Some(w) => match wire_mapping {
+                            None => w[i],
+                            Some(m) => w[m[i]],
+                        },
+                    })
+                },
+            )?;
+        }
+        for i in 0..self.r1cs.num_aux {
+            cs.alloc(
+                || format!("aux {}", i + self.aux_offset),
+                || {
+                    Ok(match witness {
+                        None => E::Fr::from_str("1").unwrap(),
+                        Some(w) => match wire_mapping {
+                            None => w[i + self.r1cs.num_inputs],
+                            Some(m) => w[m[i + self.r1cs.num_inputs]],
+                        },
+                    })
+                },
+            )?;
+        }
+
+        let make_index = |index| {
+            if index < self.r1cs.num_inputs {
+                Index::Input(index)
+            } else {
+                Index::Aux(index - self.r1cs.num_inputs + self.aux_offset)
+            }
+        };
+        let make_lc = |lc_data: Vec<(usize, E::Fr)>| {
+            lc_data
+                .iter()
+                .fold(LinearCombination::<E>::zero(), |lc: LinearCombination<E>, (index, coeff)| {
+                    lc + (*coeff, Variable::new_unchecked(make_index(*index)))
+                })
+        };
+        for (i, constraint) in self.r1cs.constraints.iter().enumerate() {
+            // 0 * LC = 0 must be ignored
+            if !((constraint.0.is_empty() || constraint.1.is_empty()) && constraint.2.is_empty()) {
+                cs.enforce(
+                    || format!("{}", i),
+                    |_| make_lc(constraint.0.clone()),
+                    |_| make_lc(constraint.1.clone()),
+                    |_| make_lc(constraint.2.clone()),
+                );
+            }
+        }
+        Ok(())
+    }
+}
+
+
+/// Our demo circuit implements this `Circuit` trait which
+/// is used during paramgen and proving in order to
+/// synthesize the constraint system.
+impl<'a, E: Engine> better_better_cs::cs::Circuit<E> for CircomCircuit<E> {
+    //noinspection RsBorrowChecker
+    fn synthesize<CS: ConstraintSystem<E>>(&self, cs: &mut CS) -> Result<(), SynthesisError> {
         let witness = &self.witness;
         let wire_mapping = &self.wire_mapping;
         for i in 1..self.r1cs.num_inputs {
