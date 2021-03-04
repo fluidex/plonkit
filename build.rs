b0da44ceb6bf6fbbf11b87cf6cc7ffe05b3b168e
Weifan <weifan@blackapi.com>
2021-03-05 01:49:43 +0800

    major: add required dep for server

---
+++ build.rs
@@ -0,0 +1,16 @@
+
+
+fn main() {
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "server")] {
+            tonic_build::configure()
+            .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+            .compile(
+                &["server.proto"], 
+                &["."],
+            )
+            .unwrap();
+            println!("cargo:rerun-if-changed=server.proto");
+        }
+    }
+}
