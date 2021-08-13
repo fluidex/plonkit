31189198408765d82f297745faac8767d077eda5
HAOYUatHZ <haoyu@protonmail.com>
2021-08-13 23:40:05 +0800

    fix

--- src/recursive.rs
+++ src/recursive.rs
@@ -7,7 +7,6 @@ use bellman_ce::SynthesisError;
 use franklin_crypto::bellman::pairing::bn256::Bn256;
 use franklin_crypto::bellman::pairing::ff::ScalarEngine;
 use franklin_crypto::bellman::pairing::{CurveAffine, Engine};
-use franklin_crypto::plonk::circuit::verifier_circuit::data_structs::IntoLimbedWitness;
 use franklin_crypto::bellman::plonk::better_better_cs::cs::PlonkCsWidth4WithNextStepAndCustomGatesParams;
 use franklin_crypto::bellman::plonk::better_better_cs::cs::ProvingAssembly;
 use franklin_crypto::bellman::plonk::better_better_cs::cs::TrivialAssembly;
@@ -20,22 +19,17 @@ use franklin_crypto::bellman::plonk::commitments::transcript::keccak_transcript:
 use franklin_crypto::bellman::worker::Worker;
 use franklin_crypto::plonk::circuit::bigint::field::RnsParameters;
 use franklin_crypto::plonk::circuit::verifier_circuit::affine_point_wrapper::aux_data::{AuxData, BN256AuxData};
+use franklin_crypto::plonk::circuit::verifier_circuit::data_structs::IntoLimbedWitness;
 use franklin_crypto::plonk::circuit::Width4WithCustomGates;
 use franklin_crypto::rescue::bn256::Bn256RescueParams;
 use itertools::Itertools;
 use recursive_aggregation_circuit::circuit::{
-    create_recursive_circuit_setup,
-    create_recursive_circuit_vk_and_setup,
-    make_aggregate, make_public_input_and_limbed_aggregate, 
-    make_vks_tree,
-    RecursiveAggregationCircuitBn256,
+    create_recursive_circuit_setup, create_recursive_circuit_vk_and_setup, create_vks_tree, make_aggregate,
+    make_public_input_and_limbed_aggregate, RecursiveAggregationCircuitBn256,
 };
 
 const VK_TREE_DEPTH: usize = 8;
 
-// TODO: 
-// assert!(index <= max_index);
-// assert!(index < 256, "for now tree should not be larger than 256 elements");
 pub fn prove(
     big_crs: Crs<Bn256, CrsForMonomialForm>,
     old_proofs: Vec<OldProof<Bn256, PlonkCsWidth4WithNextStepParams>>,
@@ -43,6 +37,7 @@ pub fn prove(
 ) -> Result<Proof<Bn256, RecursiveAggregationCircuitBn256<'static>>, SynthesisError> {
     let num_proofs_to_check = old_proofs.len();
     assert!(num_proofs_to_check > 0);
+    assert!(num_proofs_to_check < 256);
     let num_inputs = old_proofs[0].num_inputs;
     for p in &old_proofs {
         assert!(p.num_inputs == num_inputs, "proofs num_inputs mismatch!");
@@ -57,7 +52,7 @@ pub fn prove(
     let aux_data = BN256AuxData::new();
 
     let vks = old_proofs.iter().map(|_| old_vk.clone()).collect_vec();
-    let (vks_tree, all_witness_values) = make_vks_tree(&vks, &rescue_params, &rns_params);
+    let (_, (vks_tree, all_witness_values)) = create_vks_tree(&vks, VK_TREE_DEPTH)?;
     let vks_tree_root = vks_tree.get_commitment();
 
     let mut proof_ids = (0..num_proofs_to_check).collect_vec();
@@ -68,7 +63,7 @@ pub fn prove(
         let vk = &vks[proof_id];
 
         let leaf_values = vk.into_witness_for_params(&rns_params).expect("must transform into limbed witness");
-        
+
         let values_per_leaf = leaf_values.len();
         let intra_leaf_indexes_to_query: Vec<_> = ((proof_id * values_per_leaf)..((proof_id + 1) * values_per_leaf)).collect();
         let q = vks_tree.produce_query(intra_leaf_indexes_to_query, &all_witness_values);
@@ -78,20 +73,9 @@ pub fn prove(
         queries.push(q.path().to_vec());
     }
 
-    let aggregate = make_aggregate(
-        &old_proofs,
-        &vks,
-        &rescue_params,
-        &rns_params,
-    )?;
+    let aggregate = make_aggregate(&old_proofs, &vks, &rescue_params, &rns_params)?;
 
-    let (_, _) = make_public_input_and_limbed_aggregate(
-        vks_tree_root,
-        &proof_ids,
-        &old_proofs,
-        &aggregate,
-        &rns_params,
-    );
+    let (_, _) = make_public_input_and_limbed_aggregate(vks_tree_root, &proof_ids, &old_proofs, &aggregate, &rns_params);
 
     let circuit = RecursiveAggregationCircuitBn256 {
         num_proofs_to_check,
