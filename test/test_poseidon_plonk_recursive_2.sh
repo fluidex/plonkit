688ef75cecc1db8598c3e4b2873a6d7c94e9da0d
HAOYUatHZ <haoyu@protonmail.com>
2021-08-14 13:55:56 +0800

    clean up

--- test/test_poseidon_plonk_recursive_2.sh
+++ test/test_poseidon_plonk_recursive_2.sh
@@ -4,16 +4,43 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO_DIR=$DIR/".."
 CIRCUIT_DIR=$DIR"/circuits/poseidon_recursive"
 SETUP_DIR=$REPO_DIR"/keys/setup"
-SETUP_POWER=24
-SETUP_MK=$SETUP_DIR"/setup_2^${SETUP_POWER}.key"
-#SETUP_LK=$SETUP_DIR"/setup_2^${SETUP_POWER}_lagrange.key"
+SETUP_MK=$SETUP_DIR"/setup_2^20.key"
+BIG_SETUP_MK=$SETUP_DIR"/setup_2^24.key"
 DOWNLOAD_SETUP_FROM_REMOTE=false
 PLONKIT_BIN=$REPO_DIR"/target/release/plonkit"
-#PLONKIT_BIN="plonkit"
-DUMP_LAGRANGE_KEY=false
 CONTRACT_TEST_DIR=$DIR"/contract"
 
+echo "Step: build plonkit"
 cargo build --release
+
+echo "Step: universal setup"
+pushd $SETUP_DIR
+if ([ ! -f $SETUP_MK ] & $DOWNLOAD_SETUP_FROM_REMOTE); then
+  # It is the aztec ignition trusted setup key file. Thanks to matter-labs/zksync/infrastructure/zk/src/run/run.ts
+  axel -ac https://universal-setup.ams3.digitaloceanspaces.com/setup_2^${SETUP_POWER}.key -o $SETUP_MK || true
+elif [ ! -f $SETUP_MK ] ; then
+    $PLONKIT_BIN setup --power 20 --srs_monomial_form $SETUP_MK
+fi
+if ([ ! -f $BIG_SETUP_MK ] & $DOWNLOAD_SETUP_FROM_REMOTE); then
+  # It is the aztec ignition trusted setup key file. Thanks to matter-labs/zksync/infrastructure/zk/src/run/run.ts
+  axel -ac https://universal-setup.ams3.digitaloceanspaces.com/setup_2^${SETUP_POWER}.key -o $BIG_SETUP_MK || true
+elif [ ! -f $BIG_SETUP_MK ] ; then
+    $PLONKIT_BIN setup --power 24 --srs_monomial_form $BIG_SETUP_MK
+fi
+popd
+
+echo "Step: compile circuit and calculate witness"
+npx snarkit check $CIRCUIT_DIR --witness_type bin
+
+echo "Step: export verification key"
+$PLONKIT_BIN export-verification-key -m $SETUP_MK -c $CIRCUIT_DIR/circuit.r1cs -v $CIRCUIT_DIR/vk.bin
+
+echo "Step: generate each proof"
+for witness_dir in `ls $CIRCUIT_DIR/data`
+do
+  WITNESS_DIR=$CIRCUIT_DIR/data/$witness_dir
+  $PLONKIT_BIN prove -m $SETUP_MK -c $CIRCUIT_DIR/circuit.r1cs -w $WITNESS_DIR/witness.wtns -p $WITNESS_DIR/proof.bin -j $WITNESS_DIR/proof.json -i $WITNESS_DIR/public.json -t rescue
+done
 
 echo "Step: collect old_proofs"
 OLD_PROOFS_DIR=$CIRCUIT_DIR/proofs
@@ -27,10 +54,10 @@ do
 done
 
 echo "Step: export recursive vk"
-time ($PLONKIT_BIN export-recursive-verification-key -c $i -i 3 -m $SETUP_MK -v $CIRCUIT_DIR/recursive_vk.bin)
+time ($PLONKIT_BIN export-recursive-verification-key -c $i -i 3 -m $BIG_SETUP_MK -v $CIRCUIT_DIR/recursive_vk.bin)
 
 echo "Step: generate recursive proof"
-time ($PLONKIT_BIN recursive-prove -m $SETUP_MK -o $OLD_PROOFS_DIR -v $CIRCUIT_DIR/vk.bin -n $CIRCUIT_DIR/recursive_proof.bin)
+time ($PLONKIT_BIN recursive-prove -m $BIG_SETUP_MK -o $OLD_PROOFS_DIR -v $CIRCUIT_DIR/vk.bin -n $CIRCUIT_DIR/recursive_proof.bin)
 
 echo "Step: verify recursive proof"
 time ($PLONKIT_BIN recursive-verify -p $CIRCUIT_DIR/recursive_proof.bin -v $CIRCUIT_DIR/recursive_vk.bin)
