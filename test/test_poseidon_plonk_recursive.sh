1a24e00c52d5b05da36a6d4a500264a7d328cb74
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-08-21 16:33:26 +0800

    refator(recursive): read old proofs from a list (#91)
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * minor

--- test/test_poseidon_plonk_recursive.sh
+++ test/test_poseidon_plonk_recursive.sh
@@ -42,22 +42,24 @@ do
   $PLONKIT_BIN prove -m $SETUP_MK -c $CIRCUIT_DIR/circuit.r1cs -w $WITNESS_DIR/witness.wtns -p $WITNESS_DIR/proof.bin -j $WITNESS_DIR/proof.json -i $WITNESS_DIR/public.json -t rescue
 done
 
-echo "Step: collect old_proofs"
-OLD_PROOFS_DIR=$CIRCUIT_DIR/proofs
-mkdir -p $OLD_PROOFS_DIR
+echo "Step: collect old_proofs list"
+OLD_PROOF_LIST=$CIRCUIT_DIR/old_proof_list.txt
+rm $OLD_PROOF_LIST -rf
+touch $OLD_PROOF_LIST
 i=0
 for witness_dir in `ls $CIRCUIT_DIR/data`
 do
   WITNESS_DIR=$CIRCUIT_DIR/data/$witness_dir
-  cp $WITNESS_DIR/proof.bin $OLD_PROOFS_DIR/$i.proof
+  echo $WITNESS_DIR/proof.bin >> $OLD_PROOF_LIST
   let "i++"
 done
+cat $OLD_PROOF_LIST
 
 echo "Step: export recursive vk"
 time ($PLONKIT_BIN export-recursive-verification-key -c $i -i 3 -m $BIG_SETUP_MK -v $CIRCUIT_DIR/recursive_vk.bin)
 
 echo "Step: generate recursive proof"
-time ($PLONKIT_BIN recursive-prove -m $BIG_SETUP_MK -o $OLD_PROOFS_DIR -v $CIRCUIT_DIR/vk.bin -n $CIRCUIT_DIR/recursive_proof.bin)
+time ($PLONKIT_BIN recursive-prove -m $BIG_SETUP_MK -f $OLD_PROOF_LIST -v $CIRCUIT_DIR/vk.bin -n $CIRCUIT_DIR/recursive_proof.bin)
 
 echo "Step: verify recursive proof"
 time ($PLONKIT_BIN recursive-verify -p $CIRCUIT_DIR/recursive_proof.bin -v $CIRCUIT_DIR/recursive_vk.bin)
