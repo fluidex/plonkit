bfa48c4dbaec5c1f1b30776fc917ca055632c7b9
lightsing <light.tsing@gmail.com>
2021-08-23 14:51:50 +0800

    add docs

--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,7 @@ use num_bigint::BigUint;
 use num_traits::Num;
 use std::fmt::Display;
 
+/// convert a hex integer representation ("0x...") to decimal representation
 pub fn repr_to_big<T: Display>(r: T) -> String {
     BigUint::from_str_radix(&format!("{}", r)[2..], 16).unwrap().to_str_radix(10)
 }
