5e01594449a6196a2b7b50efa60ea6e0d1498ede
HAOYUatHZ <haoyu@protonmail.com>
2021-10-05 21:28:11 +0800

    clean up

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -379,23 +379,19 @@ fn prove(opts: ProveOpts) {
     proof.write(writer).unwrap();
     log::info!("Proof saved to {}", opts.proof);
 
-    cfg_if::cfg_if! {
-        if #[cfg(feature = "solidity")] {
-            let (inputs, serialized_proof) = bellman_vk_codegen::serialize_proof(&proof);
-            let ser_proof_str = serde_json::to_string_pretty(&serialized_proof).unwrap();
-            let ser_inputs_str = serde_json::to_string_pretty(&inputs).unwrap();
-            if !opts.overwrite {
-                let path = Path::new(&opts.proofjson);
-                assert!(!path.exists(), "duplicate proof json file: {}", path.display());
-                let path = Path::new(&opts.publicjson);
-                assert!(!path.exists(), "duplicate input json file: {}", path.display());
-            }
-            std::fs::write(&opts.proofjson, ser_proof_str.as_bytes()).expect("save proofjson err");
-            log::info!("Proof json saved to {}", opts.proofjson);
-            std::fs::write(&opts.publicjson, ser_inputs_str.as_bytes()).expect("save publicjson err");
-            log::info!("Public input json saved to {}", opts.publicjson);
-        }
+    let (inputs, serialized_proof) = bellman_vk_codegen::serialize_proof(&proof);
+    let ser_proof_str = serde_json::to_string_pretty(&serialized_proof).unwrap();
+    let ser_inputs_str = serde_json::to_string_pretty(&inputs).unwrap();
+    if !opts.overwrite {
+        let path = Path::new(&opts.proofjson);
+        assert!(!path.exists(), "duplicate proof json file: {}", path.display());
+        let path = Path::new(&opts.publicjson);
+        assert!(!path.exists(), "duplicate input json file: {}", path.display());
     }
+    std::fs::write(&opts.proofjson, ser_proof_str.as_bytes()).expect("save proofjson err");
+    log::info!("Proof json saved to {}", opts.proofjson);
+    std::fs::write(&opts.publicjson, ser_inputs_str.as_bytes()).expect("save publicjson err");
+    log::info!("Public input json saved to {}", opts.publicjson);
 }
 
 // verify a plonk proof by using a verification key
