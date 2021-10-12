9b0e6c021243de2d617f572ea1f0d1b4254f299f
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-10-12 22:44:20 +0800

    refactor: wrap AggregatedProof (#116)
    * wrap AggregatedProof
    
    * update
    
    * add more comments
    
    * read / write of AggregatedProof
    
    * minor
    
    * refactor
    
    * refactor
    
    * better info
    
    Co-authored-by: Ho <ho@fluidex.com>

--- src/recursive/mod.rs
+++ src/recursive/mod.rs
@@ -15,7 +15,6 @@ use franklin_crypto::bellman::plonk::better_better_cs::cs::ProvingAssembly;
 use franklin_crypto::bellman::plonk::better_better_cs::cs::TrivialAssembly;
 use franklin_crypto::bellman::plonk::better_better_cs::cs::Width4MainGateWithDNext;
 use franklin_crypto::bellman::plonk::better_better_cs::cs::{Circuit, Setup};
-use franklin_crypto::bellman::plonk::better_better_cs::proof::Proof;
 use franklin_crypto::bellman::plonk::better_better_cs::setup::VerificationKey;
 use franklin_crypto::bellman::plonk::better_better_cs::verifier::verify as core_verify;
 use franklin_crypto::bellman::plonk::commitments::transcript::keccak_transcript::RollingKeccakTranscript;
@@ -31,6 +30,9 @@ use recursive_aggregation_circuit::circuit::{
     make_public_input_and_limbed_aggregate, RecursiveAggregationCircuitBn256,
 };
 
+mod types;
+pub use types::{AggregatedProof, RecursiveVerificationKey};
+
 // only support depth<8. different depths don't really make performance different
 const VK_TREE_DEPTH: usize = 7;
 
@@ -39,12 +41,16 @@ pub fn prove(
     big_crs: Crs<Bn256, CrsForMonomialForm>,
     old_proofs: Vec<OldProof<Bn256, PlonkCsWidth4WithNextStepParams>>,
     old_vk: OldVerificationKey<Bn256, PlonkCsWidth4WithNextStepParams>,
-) -> Result<Proof<Bn256, RecursiveAggregationCircuitBn256<'static>>, SynthesisError> {
+) -> Result<AggregatedProof, SynthesisError> {
     let num_proofs_to_check = old_proofs.len();
     assert!(num_proofs_to_check > 0);
     assert!(num_proofs_to_check < 256);
+    let mut individual_vk_inputs = Vec::new();
     let num_inputs = old_proofs[0].num_inputs;
     for p in &old_proofs {
+        for input_value in p.input_values.clone() {
+            individual_vk_inputs.push(input_value);
+        }
         assert_eq!(p.num_inputs, num_inputs, "proofs num_inputs mismatch!");
     }
 
@@ -57,6 +63,7 @@ pub fn prove(
     let aux_data = BN256AuxData::new();
 
     let vks = old_proofs.iter().map(|_| old_vk.clone()).collect_vec();
+    let individual_vk_idxs = old_proofs.iter().map(|_| 0usize).collect_vec();
     let (_, (vks_tree, all_witness_values)) = create_vks_tree(&vks, VK_TREE_DEPTH)?;
     let vks_tree_root = vks_tree.get_commitment();
 
@@ -80,7 +87,7 @@ pub fn prove(
 
     let aggregate = make_aggregate(&old_proofs, &vks, &rescue_params, &rns_params)?;
 
-    let (_, _) = make_public_input_and_limbed_aggregate(vks_tree_root, &proof_ids, &old_proofs, &aggregate, &rns_params);
+    let (_, limbed_aggreagate) = make_public_input_and_limbed_aggregate(vks_tree_root, &proof_ids, &old_proofs, &aggregate, &rns_params);
 
     let circuit = RecursiveAggregationCircuitBn256 {
         num_proofs_to_check,
@@ -119,15 +126,28 @@ pub fn prove(
     circuit.synthesize(&mut assembly).expect("must synthesize");
     assembly.finalize();
 
-    assembly.create_proof::<_, RollingKeccakTranscript<<Bn256 as ScalarEngine>::Fr>>(&worker, &setup, &big_crs, None)
+    let proof = assembly.create_proof::<_, RollingKeccakTranscript<<Bn256 as ScalarEngine>::Fr>>(&worker, &setup, &big_crs, None)?;
+
+    Ok(AggregatedProof {
+        proof,
+        individual_vk_inputs,
+        individual_num_inputs: num_inputs,
+        individual_vk_idxs,
+        aggr_limbs: limbed_aggreagate,
+    })
 }
 
 // verify a recursive proof by using a corresponding verification key
 pub fn verify(
-    vk: &VerificationKey<Bn256, RecursiveAggregationCircuitBn256>,
-    proof: &Proof<Bn256, RecursiveAggregationCircuitBn256>,
+    vk: VerificationKey<Bn256, RecursiveAggregationCircuitBn256>,
+    aggregated_proof: AggregatedProof,
 ) -> Result<bool, SynthesisError> {
-    core_verify::<_, _, RollingKeccakTranscript<<Bn256 as ScalarEngine>::Fr>>(vk, proof, None)
+    let mut inputs = Vec::new();
+    for chunk in aggregated_proof.individual_vk_inputs.chunks(aggregated_proof.individual_num_inputs) {
+        inputs.push(chunk);
+    }
+    log::info!("individual_inputs: {:#?}", inputs);
+    core_verify::<_, _, RollingKeccakTranscript<<Bn256 as ScalarEngine>::Fr>>(&vk, &aggregated_proof.proof, None)
 }
 
 // export a verification key for a recursion circuit
